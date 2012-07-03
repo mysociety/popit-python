5cd7d78abfc8ea82f3cc34d7b531b6b3913827ba
Dominik Moritz <domoritz@gmail.com>
2012-07-03 11:57:51 +0200

    more tests

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from popit import *
 from slumber.exceptions import *
 import logging
 from pprint import pprint
-from oktest import test, ok
+from oktest import test, ok, NG
 import oktest
 
 logging.basicConfig(level = logging.WARN, format=FORMAT)
@@ -38,6 +38,29 @@ class GetterSetterTest(object):
 		def f():
 			self.p.sadfhoi83jhk3323
 		ok (f).raises(SchemaError)
+
+class AuthenticationTest(object):
+	@classmethod
+	def before_all(cls):
+		wrong_conf = conf.copy()
+		wrong_conf['password'] = '3h45hk345h'
+		cls.p = PopIt(**wrong_conf)
+
+	def before(self):
+		self.p = self.__class__.p
+
+	@test("wrong credentials should not fail when only reading")
+	def _(self):
+		def f():
+			self.p.person.get()
+		NG (f).raises(HttpClientError)
+
+	@test("wrong credentials should raise exception when saving")
+	def _(self):
+		
+		def f():
+			self.p.person.post({'name': 'Albert Keinstein'})
+		ok (f).raises(HttpClientError)
 
 
 class CreateTest(object):
@@ -97,7 +120,7 @@ class ReadUpdateDeleteTest(object):
 		ok(result) == True
 		def f():
 			result = self.p.person(self.id).get()
-		ok (f).raises(NotFoundError)
+		ok (f).raises(HttpClientError)
 
 	@test("cannot delete person twice")
 	def _(self):
@@ -106,7 +129,7 @@ class ReadUpdateDeleteTest(object):
 		
 		def f():
 			self.p.person(self.id).delete()
-		ok (f).raises(NotFoundError)
+		ok (f).raises(HttpClientError)
 
 		
 
