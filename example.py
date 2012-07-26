c85c9c1dd32f242e8c2724b18c2e1baef3d61ecf
Dominik Moritz <domoritz@gmail.com>
2012-07-26 22:23:52 +0200

    added __init__.py and moved example code into main function

--- example.py
+++ example.py
@@ -4,30 +4,34 @@
 from popit import PopIt
 from pprint import pprint
 
-api = PopIt(instance = 'professors', hostname = '127-0-0-1.org.uk', port = 3000, user = 'test@test.co.uk', password = 'tJo1zBum')
+def main():
+	api = PopIt(instance = 'professors', hostname = '127-0-0-1.org.uk', port = 3000, user = 'test@test.co.uk', password = 'tJo1zBum')
 
-# Create
-print("CREATE")
-new = api.person.post({'name': 'Albert Keinstein'})
-pprint(new)
+	# Create
+	print("CREATE")
+	new = api.person.post({'name': 'Albert Keinstein'})
+	pprint(new)
 
-id = new['result']['_id']
-	
-# Update
-print("UPDATE")
-result = api.person(id).put({"name": "Albert Einstein"})
-pprint(result)
+	id = new['result']['_id']
+		
+	# Update
+	print("UPDATE")
+	result = api.person(id).put({"name": "Albert Einstein"})
+	pprint(result)
 
-# Read
-print("READ")
-result = api.person(id).get()
-pprint(result)
+	# Read
+	print("READ")
+	result = api.person(id).get()
+	pprint(result)
 
-# read all
-results = api.person().get()
-pprint(results)
+	# read all
+	results = api.person().get()
+	pprint(results)
 
-# Delete
-print("DELETE")
-result = api.person(id).delete()
-pprint(result)
+	# Delete
+	print("DELETE")
+	result = api.person(id).delete()
+	pprint(result)
+
+if __name__ == '__main__':
+	main()
