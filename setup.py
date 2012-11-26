fe1e3c7fe4769fefab01f20c72784fbe09a8cf05
Steven Day <steve@mysociety.org>
2012-11-26 13:31:25 +0000

    Make setup.py read in README.rst to give PyPi a long_description

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@
 from setuptools import setup
 
 setup(name='PopIt-Python',
-	version='0.1',
+	version='0.1.2',
 	description='Python bindings to connect to the PopIt API',
+	long_description=open('README.rst', 'rt').read(),
 	author='mySociety',
 	author_email='modules@mysociety.org',
 	url='https://github.com/mysociety/popit-python',
