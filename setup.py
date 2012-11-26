bebaf1f8d41361375aaeb6be8ab544eefb1afc87
Steven Day <steve@mysociety.org>
2012-11-26 12:27:32 +0000

    Add setup.py to package PopIt_Python as a module for PyPi

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='PopIt-Python',
+	version='0.1',
+	description='Python bindings to connect to the PopIt API',
+	author='mySociety',
+	author_email='modules@mysociety.org',
+	url='https://github.com/mysociety/popit-python',
+	py_modules=['popit'],
+	install_requires=['slumber','requests']
+)
