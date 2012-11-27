d08ef4d7352684dcb6391b31bfe3c9042b41313b
Steven Day <steve@mysociety.org>
2012-11-27 13:19:20 +0000

    Add positional arguments to .format() calls to be python < 2.7 compatible

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='PopIt-Python',
-	version='0.1.4',
+	version='0.1.5',
 	description='Python bindings to connect to the PopIt API',
 	author='mySociety',
 	author_email='modules@mysociety.org',
