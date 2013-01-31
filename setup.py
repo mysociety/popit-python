1d6172ae433133b7f54fec95610515aad24f4f14
Steven Day <steve@mysociety.org>
2013-01-31 12:59:33 +0000

    Add a symlinked README for setuptools and open it into long_description for PyPi

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@
 from setuptools import setup
 
 setup(name='PopIt-Python',
-	version='0.1.6',
+	version='0.1.7',
 	description='Python bindings to connect to the PopIt API',
+        long_description=open('README', 'rt').read(),
 	author='mySociety',
 	author_email='modules@mysociety.org',
 	url='https://github.com/mysociety/popit-python',
