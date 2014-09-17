cf35edb21fc8f8c7c69a425e310f765eef53e072
Mark Longair <mhl@pobox.com>
2014-09-17 17:34:21 +0100

    Bump the version number for a new release to PyPi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='PopIt-Python',
-    version='0.1.7',
+    version='0.1.8',
     description='Python bindings to connect to the PopIt API',
         long_description=open('README', 'rt').read(),
     author='mySociety',
