e8f860d45db863c7e8cea51f881a7f5cb5d8df43
Mark Longair <mhl@pobox.com>
2014-09-17 18:23:32 +0100

    Correct the name of the module in setup.py
    ... and also bump the version number since that error broke the 0.1.8
    release.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from setuptools import setup
 
 setup(name='PopIt-Python',
-    version='0.1.8',
+    version='0.1.9',
     description='Python bindings to connect to the PopIt API',
         long_description=open('README', 'rt').read(),
     author='mySociety',
     author_email='modules@mysociety.org',
     url='https://github.com/mysociety/popit-python',
-    py_modules=['popit'],
+    py_modules=['popit_api'],
     install_requires=['requests==0.14.2','slumber']
 )
