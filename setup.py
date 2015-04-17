694b1ae052fdf7ec621c647bf06c2b941ca4ebd6
Mark Longair <mhl@pobox.com>
2015-04-17 16:04:17 +0100

    Bump the version number to 0.1.11

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='PopIt-Python',
-    version='0.1.10',
+    version='0.1.11',
     description='Python bindings to connect to the PopIt API',
         long_description=open('README.rst', 'rt').read(),
     author='mySociety',
